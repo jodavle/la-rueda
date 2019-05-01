074509d06cb2e47054cdb6ff8a52ebca6e8a2d2d
jodavle <joda.lecu@gmail.com>
2019-05-01 08:45:24 -0600

    Arreglos en header, title tag  y en estilo de página actual.

--- header.php
+++ header.php
@@ -1,7 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
     <head>
+        <meta>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="<?php if ( is_single() ) {
+            single_post_title('', true);
+        } else {
+            bloginfo('name'); echo " - "; bloginfo('description');
+        }
+        ?>" />
 	<?php wp_head(); ?>
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117952543-2"></script>
@@ -11,7 +18,29 @@
 			gtag('js', new Date());
 
 			gtag('config', 'UA-117952543-2');
-	</script>
+    </script>
+        <title><?php
+            if (is_archive()) {
+                    wp_title(''); echo ' Archivo - ';
+            } elseif (is_search()) {
+                    echo 'Buscando: &quot;'.wp_specialchars($s).'&quot; - ';
+            } elseif (!(is_404()) && (is_single()) || (is_page())) {
+                    wp_title(''); echo ' - ';
+                    } elseif (is_404()) {
+    echo 'No encontrado - ';
+}
+bloginfo('name');
+
+/*
+            $vartit = wp_title('|', true, 'right');
+            $vartot = bloginfo('name');
+            if(empty($vartit)):
+                echo($vartot);
+            elseif(empty($vartit) && ( is_home() || is_front_page() )):
+                echo(__( 'Home', 'textdomain' ) . ' | ' . get_bloginfo( 'description' ));
+            else:
+                echo($vartit . ' | ' . $vartot);
+            endif;*/ ?></title>
     </head>
     <body>
     <!-- Boot Animation -->
@@ -53,7 +82,7 @@
             <div class="col-12">
             <a class="pure-menu-heading pure-menu-link" href=""></a>
             <div class="logo"><a href="<?php echo get_home_url(); ?>"><img class="pure-img" src="
-            <?php 
+            <?php
             $custom_logo_id = get_theme_mod( 'custom_logo' );
             $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
             if ( has_custom_logo() ) {
@@ -71,4 +100,4 @@
         </div>
     </header>
 
-    
+
