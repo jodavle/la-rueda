cec23c4a18e6ad28fffd6f175da8d304058ce909
David Lechuga <joda.lecu@gmail.com>
2019-04-30 20:39:25 -0600

    Formulario a 100% de anchura

--- header.php
+++ header.php
@@ -1,7 +1,17 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <?php wp_head(); ?>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php wp_head(); ?>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117952543-2"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'UA-117952543-2');
+	</script>
     </head>
     <body>
     <!-- Boot Animation -->
